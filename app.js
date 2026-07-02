8b2eeae9a7c86fe112a512cd8e4a82eb62ce204e
Parth Agarwal <parth27092004@gmail.com>
2026-07-02 16:20:40 +0530

    version changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from CI/CD Demo v1 🚀</h1>');
+  res.send('<h1>Hello from CI/CD Demo v2 🚀</h1>');
 });
 
 app.listen(PORT, () => {
